07865c5e61a8c43072fc98be476dac997a482c36
Raphael <raphael.mazet@gmail.com>
2021-10-07 00:09:50 +0100

    Fix partner logo sizes on mobile and desktop

--- src/components/sections/Clients.js
+++ src/components/sections/Clients.js
@@ -49,11 +49,11 @@ class Clients extends React.Component {
           <div
             className={innerClasses}
           >
-            <p className="m-0">
+            <p className="mb-32">
               Ecosystem partners and integrations
             </p>
             <ul className="list-reset" style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-              <li className="reveal-rotate-from-left">
+              <li className="reveal-rotate-from-left partner-logo">
                 <a href="https://curve.fi/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/integrations/curve_logo.png')}
@@ -62,59 +62,59 @@ class Clients extends React.Component {
                     height={30} />
                 </a>
               </li>
-              <li className="reveal-rotate-from-left" data-reveal-delay="150">
+              <li className="reveal-rotate-from-left partner-logo" data-reveal-delay="150">
                 <a href="https://mstable.org/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/integrations/MTA_logo.png')}
                     alt="mStable"
-                    width={83}
+                    width={80}
                     height={30} />
                 </a>
               </li>
-              <li className="reveal-rotate-from-left" data-reveal-delay="300">
+              <li className="reveal-rotate-from-left partner-logo" data-reveal-delay="300">
                 <a href="https://sushi.com/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/integrations/SUSHI_logo.png')}
                     alt="Sushiswap"
-                    width={100}
+                    width={80}
                     height={30} />
                 </a>
               </li>
-              <li className="reveal-rotate-from-left" data-reveal-delay="450">
+              <li className="reveal-rotate-from-left partner-logo" data-reveal-delay="450">
                 <a href="https://thegraph.com/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/partnerships/THEGRAPH_logo.png')}
                     alt="The Graph"
-                    width={100}
-                    height={31}
+                    width={80}
+                    height={30}
                     style={{ borderRadius: '50%' }}
                   />
                 </a>
               </li>
-              <li className="reveal-rotate-from-left" data-reveal-delay="600">
+              <li className="reveal-rotate-from-left partner-logo" data-reveal-delay="600">
                 <a href="https://uniswap.org/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/partnerships/uniswap_pink.png')}
                     alt="Uniswap"
-                    width={100}
+                    width={80}
                     height={30} />
                 </a>
               </li>
-              <li className="reveal-rotate-from-left" data-reveal-delay="600">
+              <li className="reveal-rotate-from-left partner-logo" data-reveal-delay="600">
                 <a href="https://stakedao.org/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/integrations/STAKEDAO-logo.png')}
                     alt="stakedao"
-                    width={100}
+                    width={80}
                     height={30} />
                 </a>
               </li>
-              <li className="reveal-rotate-from-left" data-reveal-delay="600">
+              <li className="reveal-rotate-from-left partner-logo" data-reveal-delay="600">
                 <a href="https://rari.capital/" target="_blank" rel="noopener noreferrer">
                   <Image
                     src={require('./../../assets/integrations/RARI_logo.png')}
                     alt="rari"
-                    width={100}
+                    width={80}
                     height={30} />
                 </a>
               </li>
