d5b7fac2f42702061cf990c1283e68de8e2a3f05
George <gfilippou@gmail.com>
2022-03-14 14:06:24 +0200

    fix: updated position pdf links and page text copy

--- src/views/Careers.js
+++ src/views/Careers.js
@@ -34,7 +34,7 @@ class Careers extends React.Component {
     const panelDataA = {
       header: "About Our Team",
       content:
-        "SimpleFi is an award-winning DeFi analytics platform that is VC-funded, has received grants from The Graph, NEAR and Uniswap, and our team won the HackMoney EthGlobal hackathon in 2021. We are looking for great developers who love DeFi, and encourage candidates of all races, creeds, genders, age, and sexual orientation to apply.",
+        "SimpleFi is an award-winning DeFi analytics platform that is VC-funded, has received grants from The Graph, NEAR and Uniswap, and our team won the HackMoney EthGlobal hackathon in 2021. We are looking for people who love DeFi, and encourage candidates of all races, creeds, genders, age, and sexual orientation to apply.",
     };
 
     const panelDataB = {
@@ -44,28 +44,28 @@ class Careers extends React.Component {
     const jobOpenings = [
       {
         jobTitle: "Blockchain engineer (remote)",
-        link: "https://drive.google.com/file/d/1dxxBHtUjbsBmXTWKKsorUiPMl_LxYIrA/view?usp=sharing",
+        link: "https://drive.google.com/file/d/1A_CXyDf8aLsZjSLV5UjT_7zQQ4dXtqjl/view?usp=sharing",
       },
       {
         jobTitle: "Data Engineer (remote)",
-        link: "https://drive.google.com/file/d/1UZUONyiWhZqLYLqU_k3xTwshbA7Dacd8/view?usp=sharing",
+        link: "https://drive.google.com/file/d/1LEgFZtqfe6jM_EHG5JhhowiXm2F5GFKa/view?usp=sharing",
       },
       {
         jobTitle: "DevOps Engineer (remote)",
-        link: "https://drive.google.com/file/d/1WZAGO2lwyuXbrvI13nPtyoRfJAAANecw/view?usp=sharing",
+        link: "https://drive.google.com/file/d/1yZk5gV2gJZz6bNcwXjcZi4zao7JPgdGk/view?usp=sharing",
       },
 
       {
         jobTitle: "Full Stack Developer (remote)",
-        link: "https://drive.google.com/file/d/1EuLbkvX-yitAUV2yfxswhqRR2ZVweCC-/view?usp=sharing",
+        link: "https://drive.google.com/file/d/1keRi9_amSKF22Yq8P1pPFCmNfy3DI-0J/view?usp=sharing",
       },
       {
         jobTitle: "Marketing and Community Lead (remote)",
-        link: "https://drive.google.com/file/d/1VdH4jHXBI0gsleb7T3K39aFSg76vRWEA/view?usp=sharing",
+        link: "https://drive.google.com/file/d/1US0ruJ1g5lcPKK1if8HovmDcHTv7mndN/view?usp=sharing",
       },
       {
         jobTitle: "Product Manager (remote)",
-        link: "https://drive.google.com/file/d/1e9hGDY9EWfbaOx-vaIc8B8YoPYfXuiNC/view?usp=sharing",
+        link: "https://drive.google.com/file/d/17cfVWXAD2pr7Ioo2pgBUOjGR2i4Wn7D8/view?usp=sharing",
       },
     ];
 
