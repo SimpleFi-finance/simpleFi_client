61aaaf3fee0cae3e64782f2ca7271a8df51174cc
George <gfilippou@gmail.com>
2022-04-06 00:22:21 +0300

    add careers page to main menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
-import MobileHeaderLogo from './partials/MobileHeaderLogo';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import Logo from "./partials/Logo";
+import MobileHeaderLogo from "./partials/MobileHeaderLogo";
 
 const propTypes = {
   active: PropTypes.bool,
@@ -11,22 +11,21 @@ const propTypes = {
   hideNav: PropTypes.bool,
   hideSignin: PropTypes.bool,
   bottomOuterDivider: PropTypes.bool,
-  bottomDivider: PropTypes.bool
-}
+  bottomDivider: PropTypes.bool,
+};
 
 const defaultProps = {
   active: false,
-  navPosition: '',
+  navPosition: "",
   hideNav: false,
   hideSignin: false,
   bottomOuterDivider: false,
-  bottomDivider: false
-}
+  bottomDivider: false,
+};
 
 class Header extends React.Component {
-
   state = {
-    isActive: false
+    isActive: false,
   };
 
   nav = React.createRef();
@@ -34,37 +33,42 @@ class Header extends React.Component {
 
   componentDidMount() {
     this.props.active && this.openMenu();
-    document.addEventListener('keydown', this.keyPress);
-    document.addEventListener('click', this.clickOutside);
+    document.addEventListener("keydown", this.keyPress);
+    document.addEventListener("click", this.clickOutside);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyPress);
-    document.addEventListener('click', this.clickOutside);
+    document.removeEventListener("keydown", this.keyPress);
+    document.addEventListener("click", this.clickOutside);
     this.closeMenu();
   }
 
   openMenu = () => {
-    document.body.classList.add('off-nav-is-active');
-    this.nav.current.style.maxHeight = this.nav.current.scrollHeight + 'px';
+    document.body.classList.add("off-nav-is-active");
+    this.nav.current.style.maxHeight = this.nav.current.scrollHeight + "px";
     this.setState({ isActive: true });
-  }
+  };
 
   closeMenu = () => {
-    document.body.classList.remove('off-nav-is-active');
+    document.body.classList.remove("off-nav-is-active");
     this.nav.current && (this.nav.current.style.maxHeight = null);
     this.setState({ isActive: false });
-  }
+  };
 
   keyPress = (e) => {
     this.state.isActive && e.keyCode === 27 && this.closeMenu();
-  }
+  };
 
   clickOutside = (e) => {
-    if (!this.nav.current) return
-    if (!this.state.isActive || this.nav.current.contains(e.target) || e.target === this.hamburger.current) return;
+    if (!this.nav.current) return;
+    if (
+      !this.state.isActive ||
+      this.nav.current.contains(e.target) ||
+      e.target === this.hamburger.current
+    )
+      return;
     this.closeMenu();
-  }
+  };
 
   render() {
     const {
@@ -79,25 +83,23 @@ class Header extends React.Component {
     } = this.props;
 
     const classes = classNames(
-      'site-header',
-      bottomOuterDivider && 'has-bottom-divider',
+      "site-header",
+      bottomOuterDivider && "has-bottom-divider",
       className
     );
 
     return (
-      <header
-        {...props}
-        className={classes}
-      >
+      <header {...props} className={classes}>
         <div className="container">
-          <div className={
-            classNames(
-              'site-header-inner',
-              'is-revealed',
-              bottomDivider && 'has-bottom-divider'
-            )}>
+          <div
+            className={classNames(
+              "site-header-inner",
+              "is-revealed",
+              bottomDivider && "has-bottom-divider"
+            )}
+          >
             <MobileHeaderLogo />
-            {!hideNav &&
+            {!hideNav && (
               <React.Fragment>
                 <button
                   ref={this.hamburger}
@@ -111,31 +113,50 @@ class Header extends React.Component {
                 </button>
                 <nav
                   ref={this.nav}
-                  className={
-                    classNames(
-                      'header-nav is-revealed',
-                      this.state.isActive && 'is-active'
-                    )}>
+                  className={classNames(
+                    "header-nav is-revealed",
+                    this.state.isActive && "is-active"
+                  )}
+                >
                   <div className="header-nav-inner is-revealed">
-                    <ul className={
-                      classNames(
-                        'list-reset text-xxs is-revealed',
+                    <ul
+                      className={classNames(
+                        "list-reset text-xxs is-revealed",
                         navPosition && `header-nav-${navPosition}`
-                      )}>
-                      <li className='is-revealed'>
-                        <a href="https://docs.simplefi.finance" onClick={this.closeMenu}  target="_blank" rel="noopener noreferrer">Documentation</a>
+                      )}
+                    >
+                      <li className="is-revealed">
+                        <Link to="/careers">Careers</Link>
+                      </li>
+                      <li className="is-revealed">
+                        <a
+                          href="https://docs.simplefi.finance"
+                          onClick={this.closeMenu}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Documentation
+                        </a>
                       </li>
                       <li>
-                        <a href="https://github.com/SimpleFi-finance" onClick={this.closeMenu} target="_blank" rel="noopener noreferrer">Github</a>
+                        <a
+                          href="https://github.com/SimpleFi-finance"
+                          onClick={this.closeMenu}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Github
+                        </a>
                       </li>
                     </ul>
                   </div>
                 </nav>
-              </React.Fragment>}
+              </React.Fragment>
+            )}
           </div>
         </div>
       </header>
-    )
+    );
   }
 }
 
